32f3a84b8329c547073690c4033f1808e3eec554
Arif Ginanjar <arif.ginanjar@agate.id>
2020-08-29 19:44:07 +0700

    add script for load testing post API

--- locustfiles/migsappsample/locustfile.py
+++ locustfiles/migsappsample/locustfile.py
@@ -4,7 +4,15 @@ from locust import HttpUser, task, between
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
 
+    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[title]\"\r\n\r\nApi Upload\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[user_id]\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[picture]\"; filename=\"./devops.png\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+    headers = {
+        'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "fd517b48-e88b-4b28-bc6f-b9b99b39ab56"
+        }
+
     @task
     def images_page(self):
-        self.client.get("/images.json")
-#        self.client.get("/health")
+#        self.client.get("/images.json")
+        self.client.post("/images.json", data=payload, headers=headers)
