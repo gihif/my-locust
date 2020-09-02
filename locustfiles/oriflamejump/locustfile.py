bcaca9f5d1c899712072caf79d1936eb0f692251
Arif Ginanjar <arif.ginanjar@agate.id>
2020-09-02 21:04:24 +0700

    trying diferent script format for testing

--- locustfiles/oriflamejump/locustfile.py
+++ locustfiles/oriflamejump/locustfile.py
@@ -1,18 +1,17 @@
-from locust import HttpLocust, TaskSet, task
-import json, requests
+import logging
+import time
+from locust import HttpUser, task, between
 
-nameInquiry = [{
-                "score": 775,
-                "userId": "600e74742a53bf9e0e58",
-                "storeId": "test",
-                "apiVersion": "1.0"
-            }]
-myheaders = {'Content-Type': 'application/json', 'Checksum': '8f9f393012097ef1e973d4ac89c84a269550732ddd6afe207e98b18b744f7059', 'Authorization': 'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI2MDBlNzQ3NDJhNTNiZjllMGU1OCIsInN0b3JlaWQiOiJ0ZXN0IiwiZXhwIjoxNjAxNTQxMDY5fQ.IP2p7peD_Y7j-YcRPXdf6uRx6Xw3ec7hDhoDAxbnIVM'}
-
-class NameInquiries(TaskSet):
-  @task(1)
-  def send(self):
-    response = self.client.post("/Game/Voucher", data=nameInquiry, headers=myheaders)
-
-    print("Response status code:", response.status_code)
-    print("Response content:", response.text)
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2)
+    @task
+    def index_page(self):
+        newHeaders = {'Checksum': '8f9f393012097ef1e973d4ac89c84a269550732ddd6afe207e98b18b744f7059', 'Authorization': 'Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiI2MDBlNzQ3NDJhNTNiZjllMGU1OCIsInN0b3JlaWQiOiJ0ZXN0IiwiZXhwIjoxNjAxNTQxMDY5fQ.IP2p7peD_Y7j-YcRPXdf6uRx6Xw3ec7hDhoDAxbnIVM', 'Content-Type': 'application/json'}
+        mydata = {"score": 775, "userId": "600e74742a53bf9e0e58", "storeId": "test", "apiVersion": "1.0"}
+        
+        response = self.client.post("/Game/Voucher", json = mydata, headers = newHeaders)
+        if response.status_code == 400:
+            logging.info("failed : " + str(response.status_code))
+            logging.info(response.json())
+        elif response.status_code == 500:
+            logging.info("failed : internal server error" + str(response.status_code))
