72b0b098fe24c042cfdc8546c9fc62d24c710dff
Arif Ginanjar <arif.ginanjar@agate.id>
2020-09-13 13:04:01 +0700

    add more  yaml and stage predeploy

--- locustfiles/migsappsample/locustfile.py
+++ locustfiles/migsappsample/locustfile.py
@@ -1,18 +1,25 @@
+import logging
 import time
 from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
-
-    payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[title]\"\r\n\r\nApi Upload\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[user_id]\"\r\n\r\n1\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image[picture]\"; filename=\"./devops.png\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
-    headers = {
-        'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-        'Content-Type': "application/x-www-form-urlencoded",
-        'Cache-Control': "no-cache",
-        'Postman-Token': "fd517b48-e88b-4b28-bc6f-b9b99b39ab56"
-        }
-
+    
     @task
     def images_page(self):
-#        self.client.get("/images.json")
-        self.client.post("/images.json", data=payload, headers=headers)
+        self.client.get("/images.json")
+
+    @task
+    def upload_page(self):
+        newHeaders = {'Content-Type': 'application/x-www-form-urlencoded'}
+        payload = {"image[title]":"Api Upload", "image[user_id]":"2"}
+        files = [
+            ("image[picture]", open("./devops.png","rb"))
+        ]
+        
+        response = self.client.post("/images.json", headers = newHeaders, data = payload, files = files)
+        if response.status_code == 400:
+            logging.info("failed : " + str(response.status_code))
+            logging.info(response.json())
+        elif response.status_code == 500:
+            logging.info("failed : internal server error" + str(response.status_code))
