73fea4cc1a76605dcc6d4d709c49a80d059a4bb1
Arif Ginanjar <arif.ginanjar@agate.id>
2020-09-13 18:43:27 +0700

    disable development env and move to diferent folder for migsappsample

--- locustfiles/migsappsample-post/locustfile.py
+++ locustfiles/migsappsample-post/locustfile.py
@@ -4,11 +4,6 @@ from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 2)
-    
-    @task
-    def images_page(self):
-        self.client.get("/images.json")
-
     @task
     def upload_page(self):
         payload = {"image[title]": "Api Upload", "image[user_id]": "2"}
